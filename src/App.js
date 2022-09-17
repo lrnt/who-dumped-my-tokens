52ef2df1bca61022b53e5d41f9047a79afa3f602
Laurent De Marez <laurent@tokenops.xyz>
2022-09-17 21:25:07 +0200

    feat: slider

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import './App.css';
 import { useState, useEffect, useCallback, useMemo, useRef } from 'react'
 import axios from 'axios'
-import { BigNumber, ethers } from 'ethers';
-import { ForceGraph2D } from 'react-force-graph';
-
-const nodeCanvasObject = (node, ctx, globalScale) => {
-  const label = node.id;
-  const fontSize = 12 / globalScale;
-  ctx.font = `${fontSize}px Sans-Serif`;
-  const textWidth = ctx.measureText(label).width;
-  const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
-
-  ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-  ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.fillStyle = node.color;
-  ctx.fillText(label, node.x, node.y);
-
-  node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-}
+import { FixedNumber } from 'ethers';
+import { ForceGraph3D } from 'react-force-graph';
 
 
 function App() {
   const [snapshotData, setSnapshotData] = useState(undefined)
+  const [blocks, setBlocks] = useState([])
+  const [sliderValue, setSliderValue] = useState(0)
+
+  const [maxBalanceBN, setMaxBalanceBN] = useState(FixedNumber.from(0));
+  const [hoverNode, setHoverNode] = useState(null);
+
+  const snapshotIndex = blocks?.[sliderValue]
   const NODE_R = 8
+  console.log(blocks)
+
 
   useEffect(() => {
-    const retrieveEvents = async () => {
-      //const response = await axios.get('/events.json')
-      //const {events, blockEnd} = response.data
-      //setSnapshotData(getSnapshot(blockEnd, events))
-      const response = await axios.get('/snapshot.json')
-      setSnapshotData(response.data)
+    const retrieveBlocks = async () => {
+      const response = await axios.get('/snapshot-indexes.json')
+      setBlocks(response.data)
     }
-    retrieveEvents()
+    retrieveBlocks()
   }, [])
 
-  const [hoverNode, setHoverNode] = useState(null);
+  useEffect(() => {
+    if (snapshotIndex === undefined) return
+    const retrieveSnapshots = async () => {
+      const response = await axios.get(`/snapshots/${snapshotIndex}.json`)
+      setSnapshotData(response.data)
+
+      const maxBN = response.data.nodes.reduce((acc, node) => {
+        const balanceBN = FixedNumber.from(node.amount);
+        const diff = acc.subUnsafe(balanceBN);
+        return diff.isNegative() ? balanceBN : acc;
+      }, FixedNumber.from("0"));
+      setMaxBalanceBN(maxBN);
+    }
+    retrieveSnapshots()
+  }, [snapshotIndex])
 
   const handleNodeHover = node => {
     setHoverNode(node || null);
@@ -57,13 +59,40 @@ function App() {
     ctx.fill();
   }, [hoverNode]);
 
+  // DRAW COLORED RINGS
+  const drawNodeCanvas = useCallback(
+    (node, ctx) => {
+      ctx.beginPath();
+      const MIN_RADIUS = 2.0;
+      let radius = MIN_RADIUS;
+      const maxBalanceLoaded = !maxBalanceBN.isZero();
+      const nodeBalanceBN = FixedNumber.from(node.amount);
+      const nodeHasBalance = !nodeBalanceBN.isZero();
+      if (maxBalanceLoaded && nodeHasBalance) {
+        let factor = nodeBalanceBN.divUnsafe(maxBalanceBN).toUnsafeFloat();
+        const extraRadius = 10.89917 - 1.0 / (Math.sqrt(Math.sqrt(factor)) + 0.11237);
+        radius = MIN_RADIUS + extraRadius;
+      }
+      ctx.arc(node.x, node.y, radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = !nodeHasBalance ? "grey" : node === hoverNode ? "red" : "blue";
+      ctx.fill();
+    },
+    [hoverNode, maxBalanceBN]
+  );
+
+
   return (
     <>
-      {snapshotData && <ForceGraph2D
+      {blocks && (
+        <div className="w-full">
+          <input id="default-range" type="range" value={sliderValue} onChange={(e) => setSliderValue(e.target.value)} max={blocks.length} class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+        </div>
+      )}
+      {snapshotData && <ForceGraph3D
         graphData={snapshotData}
         nodeRelSize={NODE_R}
         autoPauseRedraw={false}
-        nodeCanvasObject={paintRing}
+        nodeCanvasObject={drawNodeCanvas}
         onNodeHover={handleNodeHover}
         onNodeClick={handleClick}
       />
